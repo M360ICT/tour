52347b3986af2b8b9d07d0da5705aecdc207009e
Mamun2 <mamunahmed.m360ict@gmail.com>
2023-05-02 09:40:51 +0600

    first commit

---
+++ pages/home/home_2.js
@@ -0,0 +1,292 @@
+import dynamic from "next/dynamic";
+import Seo from "../../components/common/Seo";
+import Footer2 from "../../components/footer/footer-2";
+import Header2 from "../../components/header/header-2";
+import Hero2 from "../../components/hero/hero-2";
+import CallToActions from "../../components/home/home-2/CallToActions";
+import AddBanner from "../../components/home/home-6/AddBanner";
+import Hotels from "../../components/hotels/Hotels2";
+import Tours from "../../components/tours/Tours";
+import Cars from "../../components/cars/Cars";
+import Cruise from "../../components/cruise/Cruise";
+import Testimonial from "../../components/testimonial/Testimonial";
+import CounterDark from "../../components/counter/CounterDark";
+import Brand from "../../components/brand/Brand";
+import TourCategories from "../../components/home/home-5/TourCategories";
+import BlockGuide from "../../components/home/home-6/BlockGuide";
+
+import Link from "next/link";
+
+const home_2 = () => {
+  return (
+    <>
+      <Seo pageTitle="Home-2" />
+      {/* End Page Title */}
+      <Header2 />
+      {/* End Header 2 */}
+      <Hero2 />
+      {/* End Hero 2 */}
+      <section className="layout-pt-md">
+        <div className="container">
+          <div className="row justify-center text-center">
+            <div className="col-auto">
+              <div className="sectionTitle -md">
+                <h2 className="sectionTitle__title">Special Offers</h2>
+                <p className=" sectionTitle__text mt-5 sm:mt-0">
+                  These popular destinations have a lot to offer
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* End .row */}
+          <div className="row y-gap-20 pt-40">
+            <AddBanner />
+          </div>
+          {/* End .row */}
+        </div>
+        {/* End container */}
+      </section>
+      {/* End Special Offer Section */}
+
+      <section className="layout-pt-md layout-pb-md">
+        <div className="container">
+          <div className="row y-gap-20 justify-between items-end">
+            <div className="col-auto">
+              <div className="sectionTitle -md">
+                <h2 className="sectionTitle__title">Choose Tour Types</h2>
+                <p className=" sectionTitle__text mt-5 sm:mt-0">
+                  Interdum et malesuada fames ac ante ipsum
+                </p>
+              </div>
+            </div>
+            {/* End .col */}
+
+            <div className="col-auto">
+              <div className="d-flex x-gap-15 items-center ">
+                <div className="col-auto">
+                  <button className="d-flex items-center text-24 arrow-left-hover js-tour-type-prev">
+                    <i className="icon icon-arrow-left" />
+                  </button>
+                </div>
+                {/* End prev */}
+
+                <div className="col-auto">
+                  <div className="pagination -dots text-border js-tour-type-pag" />
+                </div>
+                {/* End pagination */}
+
+                <div className="col-auto">
+                  <button className="d-flex items-center text-24 arrow-right-hover js-tour-type-next">
+                    <i className="icon icon-arrow-right" />
+                  </button>
+                </div>
+                {/* End next */}
+              </div>
+            </div>
+            {/* End .col */}
+          </div>
+          {/* End .row */}
+
+          <div className="relative overflow-hidden pt-40 sm:pt-20">
+            <TourCategories />
+          </div>
+        </div>
+      </section>
+      {/* End Tours Categories */}
+
+      <section className="layout-pt-md layout-pb-md">
+        <div className="container">
+          <div className="row y-gap-20 justify-between items-end">
+            <div className="col-auto">
+              <div className="sectionTitle -md">
+                <h2 className="sectionTitle__title">Recommended Hotels</h2>
+                <p className=" sectionTitle__text mt-5 sm:mt-0">
+                  Interdum et malesuada fames ac ante ipsum
+                </p>
+              </div>
+            </div>
+            {/* End .col */}
+
+            <div className="col-auto">
+              <Link
+                href="#"
+                className="button -md -blue-1 bg-blue-1-05 text-blue-1"
+              >
+                More <div className="icon-arrow-top-right ml-15" />
+              </Link>
+            </div>
+          </div>
+          {/* End .row */}
+
+          <div className="row y-gap-30 pt-40 sm:pt-20 item_gap-x30">
+            <Hotels />
+          </div>
+          {/* End relative */}
+        </div>
+      </section>
+      {/* End  Hotel sections */}
+      <section className="layout-pt-md layout-pb-md">
+        <div className="container">
+          <div className="row y-gap-20 justify-between items-end">
+            <div className="col-auto">
+              <div className="sectionTitle -md">
+                <h2 className="sectionTitle__title">Most Popular Tours</h2>
+                <p className=" sectionTitle__text mt-5 sm:mt-0">
+                  Interdum et malesuada fames ac ante ipsum
+                </p>
+              </div>
+            </div>
+            {/* End .col */}
+
+            <div className="col-auto">
+              <Link
+                href="#"
+                className="button -md -blue-1 bg-blue-1-05 text-blue-1"
+              >
+                More <div className="icon-arrow-top-right ml-15" />
+              </Link>
+            </div>
+            {/* End .col */}
+          </div>
+          {/* End .row */}
+
+          <div className="row y-gap-30 pt-40 sm:pt-20 item_gap-x30">
+            <Tours />
+          </div>
+          {/* End .row */}
+        </div>
+        {/* End .container */}
+      </section>
+      {/* End Tours Sections */}
+
+      <section className="layout-pt-md layout-pb-md">
+        <div className="container">
+          <div className="row y-gap-20 justify-between items-end">
+            <div className="col-auto">
+              <div className="sectionTitle -md">
+                <h2 className="sectionTitle__title">Popular Car Hire</h2>
+                <p className=" sectionTitle__text mt-5 sm:mt-0">
+                  Interdum et malesuada fames ac ante ipsum
+                </p>
+              </div>
+            </div>
+            {/* End .col */}
+
+            <div className="col-auto">
+              <Link
+                href="#"
+                className="button -md -blue-1 bg-blue-1-05 text-blue-1"
+              >
+                More <div className="icon-arrow-top-right ml-15" />
+              </Link>
+            </div>
+            {/* End .col */}
+          </div>
+          {/* End .row */}
+
+          <div className="row y-gap-30 pt-40 sm:pt-20 item_gap-x30">
+            <Cars />
+          </div>
+          {/* End .row */}
+        </div>
+        {/* End .container */}
+      </section>
+      {/* Popular Car Hire Sections */}
+
+      <section className="layout-pt-md layout-pb-md">
+        <div className="container">
+          <div className="row y-gap-20 justify-between items-end">
+            <div className="col-auto">
+              <div className="sectionTitle -md">
+                <h2 className="sectionTitle__title">Featured Cruise Deals</h2>
+                <p className=" sectionTitle__text mt-5 sm:mt-0">
+                  Interdum et malesuada fames ac ante ipsum
+                </p>
+              </div>
+            </div>
+            {/* End .col */}
+
+            <div className="col-auto">
+              <Link
+                href="#"
+                className="button -md -blue-1 bg-blue-1-05 text-blue-1"
+              >
+                More <div className="icon-arrow-top-right ml-15" />
+              </Link>
+            </div>
+            {/* End .col */}
+          </div>
+          {/* End .row */}
+
+          <div className="row y-gap-30 pt-40 sm:pt-20 item_gap-x30">
+            <Cruise />
+          </div>
+          {/* End .row */}
+        </div>
+        {/* End .container */}
+      </section>
+      {/* Features Cruise Deals Sections */}
+
+      <section className="layout-pt-lg layout-pb-lg bg-dark-1">
+        <div className="container">
+          <div className="row justify-center text-center">
+            <div className="col-auto">
+              <div className="sectionTitle text-white">
+                <h2 className="sectionTitle__title">Customer Reviews</h2>
+                <p className=" sectionTitle__text mt-5 sm:mt-0">
+                  These popular destinations have a lot to offer
+                </p>
+              </div>
+            </div>
+          </div>
+          {/* End .row */}
+
+          <div className="overflow-hidden pt-60 lg:pt-40 sm:pt-30">
+            <div className="item_gap-x30">
+              <Testimonial />
+            </div>
+          </div>
+          {/* End .overflow-hidden */}
+
+          <div className="row y-gap-30 items-center pt-40 sm:pt-20">
+            <div className="col-xl-4">
+              <CounterDark />
+            </div>
+            {/* End .col */}
+
+            <div className="col-xl-8">
+              <div className="row y-gap-30 justify-between items-center">
+                <Brand />
+              </div>
+            </div>
+            {/* End .col */}
+          </div>
+          {/* End .row */}
+        </div>
+        {/* End .container */}
+      </section>
+      {/* End testimonial section */}
+
+      {/* <AppBanner /> */}
+      {/* App Banner Section */}
+
+      <section className="layout-pt-md layout-pb-md bg-light-2">
+        <div className="container">
+          <div className="row y-gap-30">
+            <BlockGuide />
+          </div>
+          {/* End .row */}
+        </div>
+        {/* End .container */}
+      </section>
+      {/* End Block Guide */}
+
+      <CallToActions />
+      {/* End CallToActions Section */}
+      <Footer2 />
+      {/* End Footer Section */}
+    </>
+  );
+};
+
+export default dynamic(() => Promise.resolve(home_2), { ssr: false });
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Wrapper from "./layout/wrapper";
+import Home2 from "./home/home_2";
+
+const MainRoot = () => {
+  return (
+    <Wrapper>
+      <Home2 />
+    </Wrapper>
+  );
+};
+
+export default MainRoot;
